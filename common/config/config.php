bffbf33125819bd3cee4f8c6f1ddb7bc2f5bd80e
Kevin Zhao <follow_kevin@126.com>
2020-08-04 20:09:17 +0800

    'productiondb'

--- common/config/config.php
+++ common/config/config.php
@@ -13,8 +13,10 @@ return array(
     'db_type' => 'pdo',     	    // 数据库链接扩展 , 支持 pdo | mysqli | mysql
     'db_host' => '127.0.0.1',       // 服务器地址
     'db_name' => 'yzmcms',		    // 数据库名
-    'db_user' => 'root',            // 用户名
-    'db_pwd' => '',       	// 密码
+    'db_user' => 'yzmcms',            // 用户名
+    'db_pwd' => '!+_[pY{tIZfk',       	// 密码
+   // 'db_user' => 'yzmcms',            // 用户名
+   // 'db_pwd' => '!+_[pY{tIZfk',       	// 密码
     'db_port' => 3306,              // 端口
     'db_prefix' => 'yzm_',          // 数据库表前缀
     
